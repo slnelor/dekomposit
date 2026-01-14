38fb50698a661f9f3e7d4e312a3b4cc9cc1e2ada
MikeMitusov <mitsivimail@gmail.com>
2026-01-14 15:24:07 +0200

    Rewrite client var to Client class

--- novamova/llm/base_client.py
+++ novamova/llm/base_client.py
@@ -1,12 +1,62 @@
+import os
+import logging
+from functools import lru_cache
+
 from openai import AsyncOpenAI
+from pydantic import BaseModel
 
-from novamova.config import cfg
+from novamova.config import DEFAULT_LLM, DEFAULT_SERVER, CURRENT_API_KEY, LLM_CONFIG
 
 
-DEFAULT_LLM = "gemini-flash-lite-latest"
-DEFAULT_SERVER = "https://generativelanguage.googleapis.com/v1beta/openai/"
+logging.basicConfig(
+    datefmt="%d/%m/%Y %H:%M",
+    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-LLM_SERVER = cfg.get("LLM_SERVER", DEFAULT_SERVER)
-LLM_MODEL = cfg.get("LLM_MODEL", DEFAULT_LLM)
+LLM_SERVER = os.getenv("LLM_SERVER", DEFAULT_SERVER)
+if LLM_SERVER.lower() in ["none", "null"]:
+    LLM_SERVER = None
 
-client = AsyncOpenAI(api_key=cfg["LLMAPI_KEY"], base_url=LLM_SERVER)
+LLM_MODEL = os.getenv("LLM_MODEL", DEFAULT_LLM)
+
+
+class Client:
+    """An AsyncOpenAI wrapper for novamova service"""
+
+    def __init__(
+        self,
+        model: str | None = None,
+        server: str | None = None,
+    ) -> None:
+        self.model = model or LLM_MODEL
+        self.server = server or LLM_SERVER
+
+    @staticmethod
+    @lru_cache
+    def client() -> AsyncOpenAI:
+        return AsyncOpenAI(api_key=LLM_CONFIG[CURRENT_API_KEY], base_url=LLM_SERVER)
+
+    async def request(
+        self,
+        messages: list[dict],
+        return_format: type[BaseModel],
+        model: str | None = None,
+    ):
+        """A method wrapper around openai.chat.completions.parse"""
+        model = model or self.model
+
+        response = await self.client().chat.completions.parse(
+            model=model,
+            messages=messages,
+            response_format=return_format,
+        )
+
+        usage = response.usage
+        logger.info(
+            f"LLMCALL {response.id}\tTOKENS USED: {usage.prompt_tokens} (input); "
+            f"{usage.completion_tokens} (output); {usage.total_tokens} (total)"
+        )
+
+        return response
